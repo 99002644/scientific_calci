85c80a0d1d5264a5009c2716c5f34b7a27140613
99002644 <71483892+99002644@users.noreply.github.com>
2020-09-19 13:38:21 +0530

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,1 +1,19 @@
 
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+float add(float,float);
+float sub(float,float);
+float mul(float,float);
+float div(float,float);
+float squareroot(float);
+float sine(float);
+float cosine(float);
+float tangent(float);
+float inv(float);
+float power(float,float);
+float logarithm(float);
+float square(float);
